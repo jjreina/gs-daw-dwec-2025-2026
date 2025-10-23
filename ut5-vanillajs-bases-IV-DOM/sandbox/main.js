7a57299cd0ebf95741fc96dd5ef0750ece60dd92
jjreina <jreinapascual@gmail.com>
2025-10-23 10:11:49 +0200

    feat: add index.html and main.js for initial DOM manipulation and content display

---
+++ ut5-vanillajs-bases-IV-DOM/sandbox/main.js
@@ -0,0 +1,4 @@
+const coment = document.createComment('Comentario de ejemplo');
+const textNode = document.createTextNode('Hola Mundo');
+document.body.appendChild(coment);
+document.body.appendChild(textNode);
