0920649541c23ff1878024164e432193136ed350
jjreina <jreinapascual@gmail.com>
2026-02-03 18:26:20 +0100

    feat: implement province resource and update app structure

--- ut8-angular/10-httpresource/src/app/app.ts
+++ ut8-angular/10-httpresource/src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProvinceResponse } from './models/province';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('10-httpresource');
+  protected provincesResource = httpResource<ProvinceResponse>(
+    () => 'http://localhost:3000/provinces',
+  );
 }
---
+++ ut8-angular/10-httpresource/src/app/models/province.ts
@@ -0,0 +1,8 @@
+export interface ProvinceResponse {
+  total_count: number;
+  results: Province[];
+}
+export interface Province {
+  ccaa: string;
+  provincia: string;
+}
