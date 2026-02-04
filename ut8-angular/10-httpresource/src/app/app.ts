43f7ea2ffac7cf418fb3b44d052ad106f1fd509f
jjreina <jreinapascual@gmail.com>
2026-02-04 16:06:42 +0100

    feat: add movie fetching functionality and display in app

--- ut8-angular/10-httpresource/src/app/app.ts
+++ ut8-angular/10-httpresource/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProvinceResponse } from './models/province';
 import { httpResource } from '@angular/common/http';
+import { Movie } from './models/movies';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,19 @@ export class App {
   protected provincesResource = httpResource<ProvinceResponse>(
     () => 'http://localhost:3000/provinces',
   );
+
+  readonly url = 'https://imdb236.p.rapidapi.com/api/imdb/most-popular-movies';
+  readonly options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': '6185a78aa2mshb04ae8991085691p1d093bjsnae8623070f4a',
+      'x-rapidapi-host': 'imdb236.p.rapidapi.com',
+    },
+  };
+
+  protected fecthMovies = httpResource<Movie[]>(() => ({
+    url: this.url,
+    method: this.options.method,
+    headers: this.options.headers,
+  }));
 }
---
+++ ut8-angular/10-httpresource/src/app/models/movies.ts
@@ -0,0 +1,5 @@
+export interface Movie {
+  id: string;
+  primaryTitle: string;
+  originalTitle: string;
+}
