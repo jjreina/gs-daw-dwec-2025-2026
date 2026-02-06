8a90e4ca60df86ba8d78afd497764a84084ecab9
jjreina <jreinapascual@gmail.com>
2026-02-06 16:47:11 +0100

    feat: initialize Angular application with routing and basic structure
    - Add favicon.ico for the application.
    - Create app configuration file to provide global error listeners and router.
    - Set up main application component with routing outlet.
    - Define application routes in a separate file.
    - Create basic styles and HTML structure for the app.
    - Implement an 'animes' feature with its own component, styles, and HTML.
    - Add AnimeApi service for future API interactions.
    - Set up index.html as the entry point for the application.
    - Configure TypeScript settings for the application and tests.

---
+++ ut8-angular/11-httpclient/src/app/features/animes/animes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animes',
+  imports: [],
+  templateUrl: './animes.html',
+  styleUrl: './animes.css',
+})
+export class Animes {
+
+}
