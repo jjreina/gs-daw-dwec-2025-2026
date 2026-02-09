87873abe4cf332dfe0f5624b325d04804f696081
jjreina <jreinapascual@gmail.com>
2026-02-09 17:01:42 +0100

    feat: add edit functionality for anime with UI and API integration

--- ut8-angular/11-httpclient/src/app/features/animes/animes.ts
+++ ut8-angular/11-httpclient/src/app/features/animes/animes.ts
@@ -11,6 +11,25 @@ import { AnimeApi } from './services/anime-api';
 export class Animes {
   protected readonly animesApi: AnimeApi = inject(AnimeApi);
 
+  protected editAnimeById(id: number) {
+    let newEpisodes = prompt('Edit the number of episodes for this anime');
+    if (newEpisodes === null) return; // User cancelled the prompt
+    const episodesNumber = parseInt(newEpisodes, 10);
+    if (isNaN(episodesNumber)) {
+      alert('Please enter a valid number');
+      return;
+    }
+    this.animesApi.updateAnimeById(id, episodesNumber).subscribe({
+      next: () => {
+        this.animesApi.getAllAnimes.reload();
+        console.log('Anime updated successfully');
+      },
+      error: (err) => {
+        console.error('Error updating the anime', err);
+      },
+    });
+  }
+
   protected deleteAnimeById(id: number) {
     if (!confirm('¿Seguro que quieres eliminar este anime?')) return;
     this.animesApi.deleteAnimeById(id).subscribe({
--- ut8-angular/11-httpclient/src/app/features/animes/services/anime-api.ts
+++ ut8-angular/11-httpclient/src/app/features/animes/services/anime-api.ts
@@ -8,10 +8,16 @@ import { Observable } from 'rxjs';
 })
 export class AnimeApi {
   private http: HttpClient = inject(HttpClient);
+
   private readonly url: string = 'http://localhost:3000/animes';
 
   public readonly getAllAnimes = httpResource<Anime[]>(() => this.url);
+
   public readonly deleteAnimeById = (idAnime: number): Observable<void> => {
     return this.http.delete<void>(`${this.url}/${idAnime}`);
+  };
+
+  public readonly updateAnimeById = (idAnime: number, newEpisodes: number): Observable<Anime> => {
+    return this.http.patch<Anime>(`${this.url}/${idAnime}`, { episodes: newEpisodes });
   };
 }
